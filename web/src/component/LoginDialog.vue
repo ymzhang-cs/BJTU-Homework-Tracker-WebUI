d3702b6e6fbd3df9921ea2ce1879c844a2c0f88f
rambert <189076903+ram6ert@users.noreply.github.com>
2024-12-02 23:54:16 +0800

    feat: login via mis

---
+++ web/src/component/LoginDialog.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from "vue";
+
+const dialog = defineModel('dialog')
+
+const selection = ref(null);
+
+function login() {
+  if(selection.value === 'MIS') {
+    pywebview.api.loginViaMis();
+  }
+}
+
+</script>
+
+<template>
+  <v-dialog v-model="dialog">
+    <v-container>
+      <v-card>
+        <v-card-title>登录</v-card-title>
+        <v-card-text>
+          <v-select label="登录方式" :items="['MIS']" v-model="selection"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn text="确定" variant="tonal" @click="login"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </v-dialog>
+</template>
+
+<style scoped>
+
+</style>
--- web/src/component/NavigationDrawer.vue
+++ web/src/component/NavigationDrawer.vue
@@ -1,14 +1,17 @@
 <script setup>
 import {useDisplay} from "vuetify";
 import {computed, onMounted, ref} from "vue";
+import LoginDialog from "./LoginDialog.vue";
 
 const hidden = defineModel('hidden', {type:Boolean, default: false});
+const dialog = ref(false);
 const items = [
-  {title: '通过MIS登录', onclick: () => pywebview.api.loginViaMis()}
+  {title: '登录', onclick: () => dialog.value = true},
 ];
 </script>
 
 <template>
+  <LoginDialog v-model:dialog="dialog"></LoginDialog>
   <v-navigation-drawer v-model="hidden" location="left" temporary>
     <v-list>
       <v-list-item
